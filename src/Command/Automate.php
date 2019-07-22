656572774178ca2cd28d4b8a3f4357a2b65d0605
Mehdi BOISSAT-BRON <mehdi.boissat@smile.fr>
2019-07-22 15:13:54 +0200

    #5 Add new features first part

--- src/Command/Automate.php
+++ src/Command/Automate.php
@@ -3,6 +3,7 @@
 
 namespace App\Command;
 
+use App\Object\SevenZero;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -18,6 +19,25 @@ class Automate extends Command
 
     public function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln("This is my command");
+        $sevenZero = new SevenZero();
+
+        //Scalar type declarations
+        $output->writeln($sevenZero->scalarSum(1, 5));
+
+        //Return type declarations
+        $output->writeln($sevenZero->sum(1, 2));
+        $output->writeln($sevenZero->sommeTableaux([1,2,3], [4,5,6], [7,8,9]));
+
+        //Null coalescing operator
+        $identifiant = $_GET['utilisateur'] ?? 'Nothing';
+        $output->writeln($identifiant);
+
+        $identifiant = $_GET['utilisateur'] ?? $_POST['utilisateur'] ?? 'Nothing';
+        $output->writeln($identifiant);
+
+        //Spaceship operator
+        $output->writeln(1 <=> 1);
+        $output->writeln(1 <=> 2);
+        $output->writeln(2 <=> 1);
     }
 }
