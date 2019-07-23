b900873141d25e367dab835ddb3439549585923b
Mehdi BOISSAT-BRON <mehdi.boissat@smile.fr>
2019-07-23 16:37:07 +0200

    #11 Implement third command

--- console.php
+++ console.php
@@ -4,8 +4,10 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Symfony\Component\Console\Application;
 use App\Command\SevenZero;
 use App\Command\SevenOne;
+use App\Command\SevenTwo;
 
 $app = new Application('Console App', 'v1.0.0');
 $app->add(new SevenZero());
 $app->add(new SevenOne());
+$app->add(new SevenTwo());
 $app->run();
---
+++ src/Definition/classB.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Definition;
+
+
+class classB implements interfaceA
+{
+    public function Test($input){}
+}
